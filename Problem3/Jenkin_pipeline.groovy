538a4d0d375cafe0dbfc8fbb109500e5789584a8
Farook Ali <92211338+farookizn@users.noreply.github.com>
2022-10-10 12:34:20 +0100

    Problem3

---
+++ Problem3/Jenkin_pipeline.groovy
@@ -0,0 +1,49 @@
+#!groovy
+
+pipeline {
+	agent none  stages {
+  	stage('Maven Install') {
+    	agent {
+      	docker {
+        	image 'Bitcoin:0.15.0'
+        }
+      }
+      steps {
+      	sh 'mvn clean install'
+      }
+    }
+    stage('Docker Build') {
+    	agent any
+      steps {
+      	sh 'docker build -t farook/bitcoind:latest .'
+      }
+    }
+    stage('Docker Push') {
+    	agent any
+      steps {
+      	withCredentials([usernamePassword(credentialsId: 'dockerHub', passwordVariable: 'dockerHubPassword', usernameVariable: 'dockerHubUser')]) {
+        	sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPassword}"
+          sh 'docker push farook/bitcoind:latest'
+        }
+      }
+    }
+
+    stage('Deploy - Staging') {
+            steps {
+                sh './deploy staging'
+                sh './run-smoke-tests'
+            }
+        }
+
+        stage('Sanity check') {
+            steps {
+                input "Does the staging environment look ok?"
+            }
+        }
+
+        stage('Deploy - Production') {
+            steps {
+                sh './deploy production'
+            }
+  }
+}
